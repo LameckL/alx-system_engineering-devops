a53af5681a791f3b1c1450c88a2634d4d35c384b
LameckL <88710250+LameckL@users.noreply.github.com>
2024-01-07 15:49:20 +0000

    fix0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-# create file
 
-file { 'tmp/school':
-	content => 'I love puppet'.
-	mode => '0744'.
-	owner => 'www-data'.
-	group => 'www-data'.
+# creates a file in /tmp
+
+file {'/tmp/school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
