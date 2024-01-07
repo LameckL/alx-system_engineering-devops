baf42ab911ee2095ee4cc7ca5e902da4a9b97500
LameckL <88710250+LameckL@users.noreply.github.com>
2024-01-07 16:14:44 +0000

    fixing1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#!/usr/bin/pup
-# install flask version 2.1.0
-package { 'flask':
-	ensure => '2.1.0'.
-	provider => 'pip3'.
+# installs puppet-lint package
+
+package {'flask':
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3'
 }
